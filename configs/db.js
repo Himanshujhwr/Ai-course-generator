aa0f77bf76c02724ac101b78c9a8fe9953c5f695
Your Name <you@example.com>
2026-01-03 19:07:07 +0530

    fix: disable prerendering for db pages

--- configs/db.js
+++ configs/db.js
@@ -1,9 +1,8 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
+// import { neon } from '@neondatabase/serverless';
+// import { drizzle } from 'drizzle-orm/neon-http';
 
-const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DB_URL);
-export const db = drizzle(sql);
-
+// const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DB_URL);
+// export const db = drizzle(sql);
 
 // import { neon } from "@neondatabase/serverless";
 // import { drizzle } from "drizzle-orm/neon-http";
@@ -17,3 +16,12 @@ export const db = drizzle(sql);
 
 // const sql = neon(dbUrl);
 // export const db = drizzle(sql, { schema });
+import { neon } from "@neondatabase/serverless";
+
+const databaseUrl = process.env.NEXT_PUBLIC_DRIZZLE_DB_URL;
+
+if (!databaseUrl) {
+  throw new Error("DATABASE URL not found");
+}
+
+export const db = neon(databaseUrl);
